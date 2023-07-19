fb66ce3a73e033cc3c05dd254394a854520e8596
JaeHan-Kim <hgal8877@gmail.com>
2023-07-19 22:12:45 +0900

    test Set Class

---
+++ src/test/java/study/SetTest.java
@@ -0,0 +1,44 @@
+package study;
+
+import org.assertj.core.util.Strings;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SetTest {
+    private Set<Integer> numbers;
+
+    @BeforeEach
+    void setUp() {
+        numbers = new HashSet<>();
+        numbers.add(1);
+        numbers.add(1);
+        numbers.add(2);
+        numbers.add(3);
+    }
+
+    // Test Case 구현
+    @Test
+    void 요구사항1_size체크() {
+        assertThat(numbers.size()).isEqualTo(3);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {1, 2, 3})
+    void 요구사항2_contain(int value) {
+        assertThat(numbers.contains(value)).isTrue();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {"1,true", "2,true", "3,true", "4,false", "5,false"})
+    void 요구사항3_csv_test(int value, boolean flag) {
+        assertThat(numbers.contains(value)).isEqualTo(flag);
+    }
+}
