cc4e2c6d0e668c7cbbe0797b41c7d01efee4dd2f
soso1yun-at-522339398443 <zRKAKNnlV/Wv5HpoLWLVokbcebhW2ZErcyhCMzygReU=>
2023-07-19 21:56:47 +0900

    미션실습 환경 구축 - String 클래스에 대한 학습 테스트

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,29 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 
 public class StringTest {
     @Test
     void replace() {
-        String actual = "abc".replace("b", "d");
-        assertThat(actual).isEqualTo("adc");
+        String actual = "abc";
+
+        String numStr = "1,2";
+        String[] splitNum = numStr.split(",");
+        String[] splitFirstNum = new String[]{splitNum[0]};
+
+        assertThat(splitNum).contains("1");
+        assertThat(splitNum).contains("2");
+        assertThat(splitFirstNum).containsExactly("1");
+
+        assertThat("(1,2)".substring(1,4)).isEqualTo("1,2");
+
+
+        assertThatThrownBy(() -> {
+            actual.charAt(4);
+        }).isInstanceOf(IndexOutOfBoundsException.class)
+                .hasMessageContaining("Index: 2, Size:2");
+
+
     }
 }
