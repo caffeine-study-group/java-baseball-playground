599b59f5853a4396df7c8855b733653543ce5d24
gosuljo <lurutia@naver.com>
2023-07-19 21:47:06 +0900

    [TEST] 요구사항 3 Exception Assertion 테스트 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -1,8 +1,9 @@
 package study;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringTest {
@@ -28,5 +29,33 @@ public class StringTest {
         int beginIndex = str.indexOf("(");
         int endIndex = str.indexOf(")");
         assertThat(str.substring(beginIndex + 1, endIndex)).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt_1() {
+        String str = "abc";
+        assertThat(str.charAt(0)).isEqualTo('a');
+        assertThat(str.charAt(1)).isEqualTo('b');
+        assertThat(str.charAt(2)).isEqualTo('c');
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException 발생 테스트 1")
+    void charAt_2() {
+        String str = "abc";
+        assertThatThrownBy(() -> {
+            str.charAt(3);
+        }).isInstanceOf(StringIndexOutOfBoundsException.class)
+                .hasMessageContaining("String index out of range: 3");
+    }
+
+    @Test
+    @DisplayName("StringIndexOutOfBoundsException 발생 테스트 2")
+    void charAt_3() {
+        String str = "abc";
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+        .isThrownBy(() -> {
+            str.charAt(3);
+        }).withMessageMatching("String index out of range: \\d+");
     }
 }
