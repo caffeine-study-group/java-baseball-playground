11417d00229d8d341d2d99c554a08e03b2828df2
gosuljo <lurutia@naver.com>
2023-07-19 21:31:02 +0900

    [TEST] 요구사항 1 split 테스트 추가

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,22 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split_1() {
+        assertThat("1,2".split(",")).contains("1", "2");
+    }
+
+    @Test
+    void split_2() {
+        assertThat("1".split(",")).containsExactly("1");
     }
 }
